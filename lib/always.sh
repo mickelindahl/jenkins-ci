78c2b5ba1819806c0cd30c9b106ff2be62e96a75
Mikael Lindahl <mikael.lindahl.07@gmail.com>
2018-01-31 15:17:27 +0100

    fix?

--- lib/always.sh
+++ lib/always.sh
@@ -3,6 +3,8 @@
 IMAGES=$(docker images -f 'dangling=true' -q --no-trunc);
 IMAGES=$(echo $IMAGES | tr '\n' ' ')
 
+echo "IMAGES: $IMAGES"
+
 if [ ! "$IMAGES" = "" ]; then
 
     docker rmi ${IMAGES};
