3218c1bd04880e28c5c725c56829da7a8c1609e6
Mikael Lindahl <mikael.lindahl.07@gmail.com>
2018-01-31 15:23:26 +0100

    fix?

--- lib/always.sh
+++ lib/always.sh
@@ -3,7 +3,7 @@
 IMAGES=$(docker images -f 'dangling=true' -q --no-trunc);
 IMAGES=$(echo $IMAGES | tr '\n' ' ')
 
-echo "IMAGES: $IMAGES"
+echo "IMAGES:i-$IMAGES-i"
 
 if [ ! "$IMAGES" = "" ]; then
 
