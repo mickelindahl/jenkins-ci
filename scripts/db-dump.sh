e1f501f348abe30925845fb89461c2d67b461a67
Mikael Lindahl <mikael.lindahl.07@gmail.com>
2019-09-21 19:26:08 +0200

    postgresql 9.6 -> 10.10

--- scripts/db-dump.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-CONTAINER=$1
-
-# Backup:
-docker exec -t -u postgres $CONTAINER pg_dumpall -c > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-
--- scripts/db-restore.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-$DUMP=$1
-$CONTAINER=$2
-
-# Restore:
-cat $DUMP | docker exec -i $CONTAINER psql -Upostgres
-
-
